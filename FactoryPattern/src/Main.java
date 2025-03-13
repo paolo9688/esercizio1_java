ae1b9ab582409f69b201d925594c2ae0e2804dcd
paolo9688 <paolo.pz1@gmail.com>
2025-03-13 09:42:30 +0100

    fix: aggiunti i commenti nella classe Main

--- FactoryPattern/src/Main.java
+++ FactoryPattern/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        // Creo un oggetto cerchio e stampo la sua forma:
         Shape cerchio = ShapeFactory.getForma(ShapeList.CERCHIO);
         cerchio.Draw();
+        // Creo un oggetto rettangolo e stampo la sua forma:
         Shape rettangolo = ShapeFactory.getForma(ShapeList.RETTANGOLO);
         rettangolo.Draw();
+        // Creo un oggetto quadrato e stampo la sua forma:
         Shape quadrato = ShapeFactory.getForma(ShapeList.QUADRATO);
         quadrato.Draw();
     }
